c066be88aa8dea306bdbd7123cdf81c2508f4055
aliciaaml <90557106+aliciaaml@users.noreply.github.com>
2023-07-05 09:45:41 +0200

    PanelFinal

---
+++ Assets/Tablero/Casillas/CASILLAFINAL.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CASILLAFINAL : MonoBehaviour
+{
+    public GameObject pantallaFinal;
+    public GameObject dado;
+    public GameObject textoPlayer;
+    public GameObject setings;
+    public GameObject textoDados;
+    private void OnTriggerEnter(Collider other)
+    {
+        pantallaFinal.SetActive(true);
+        dado.SetActive(false);
+        textoPlayer.SetActive(false);
+        setings.SetActive(false);
+        textoDados.SetActive(false);
+
+    }
+
+}
